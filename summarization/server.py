14063ffe56d8cde7d54abbd5be0ea07a65f97658
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-10-13 19:36:27 -0300

    Change LSA Summarizer to Text rank

--- summarization/server.py
+++ summarization/server.py
@@ -1,18 +1,19 @@
 from __future__ import absolute_import
 from __future__ import division, print_function, unicode_literals
 
-from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.summarizers.text_rank import TextRankSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 from flask import Flask, request, jsonify, Response
 from pymongo import MongoClient
+from datetime import datetime, timedelta
 import nltk
 import string
 import re
 import json
+import dateutil.parser
 
 nltk.download('punkt')
 
@@ -22,7 +23,6 @@ client = MongoClient('mongo', 27017)
 db = client['summarization']
 
 LANGUAGE = "portuguese"
-SENTENCES_COUNT = 2
 stemmer = Stemmer(LANGUAGE)
 
 summarizer = Summarizer(stemmer)
@@ -32,6 +32,8 @@ summarizer.stop_words = get_stop_words(LANGUAGE)
 def save_message():
     messages = db['messages']
     message_to_save = request.json
+    message_to_save['createdAt'] = dateutil.parser.parse(message_to_save['createdAt']) - timedelta(hours=3)
+    message_to_save['updatedAt'] = dateutil.parser.parse(message_to_save['updatedAt']) - timedelta(hours=3)
     remove = string.punctuation
     pattern = r"[{}]".format(remove)
     message_to_save['text'] = re.sub(pattern, '', message_to_save['text'])
@@ -40,8 +42,11 @@ def save_message():
 
 @app.route('/summary', methods=['GET'])
 def get_summary():
+    frequency = request.args.get('frequency')
+    query = get_filter_by_date(frequency)
     messages = db['messages']
-    documents = messages.find({})
+    documents = messages.find(query)
+    SENTENCES_COUNT = fourty_percent(documents.count())
     chunks = []
     for document in documents:
         chunks.append(document['text'] + '. ')
@@ -52,6 +57,27 @@ def get_summary():
         sentence_chunks.append(str(sentence))
     return Response(json.dumps({"sentences": sentence_chunks}), mimetype='application/json')
 
+def get_filter_by_date(frequency):
+    if frequency == 'every_minute':
+        return {
+            'updatedAt': { '$lte': datetime.now(), '$gte': datetime.today() - timedelta(minutes=1) }
+        }
+    elif frequency == 'every_day_at_12':
+        return {
+            'updatedAt': { '$lte': datetime.now(), '$gte': datetime.today() - timedelta(hours=24) }
+        }
+    elif frequency == 'every_wed':
+        return {
+            'updatedAt': { '$lte': datetime.now(), '$gte': datetime.today() - timedelta(days=7) }
+        }
+    elif frequency == 'every_12':
+        return {
+            'updatedAt': { '$lte': datetime.now(), '$gte': datetime.today() - timedelta(hours=12) }
+        }
+    return {}
+
+def fourty_percent(quantity):
+    return int(round((40 * quantity)/100))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
