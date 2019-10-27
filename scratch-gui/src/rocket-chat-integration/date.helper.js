367176ced0a0cedea467c41165d1e25ab16b11cc
Marcos Defendi <marcos.defendi@ulbra.inf.br>
2019-10-27 19:02:04 -0300

    fix hour

--- scratch-gui/src/rocket-chat-integration/date.helper.js
+++ scratch-gui/src/rocket-chat-integration/date.helper.js
@@ -2,7 +2,7 @@ import moment from 'moment';
 
 export const getShift = (hour) => {
 	const morning = moment('7:00', 'h:mma');
-	const afternoon = moment('11:30', 'h:mma');
+	const afternoon = moment('12:00', 'h:mma');
 	const night = moment('18:00', 'h:mma');
 	if(hour.isSame(morning) || (hour.isAfter(morning) && hour.isBefore(afternoon))){
 		return 'manha';
